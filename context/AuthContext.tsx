5e9a6ee84b6b5257a9e5426cde4b871cd1eec770
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-02-04 15:58:13 -0300

    headers na requisicao

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { api } from "../services/api";
 import { useRouter } from "next/router";
-
+import { setCookie, parseCookies } from "nookies";
 type SignInCredentials = {
     email: string;
     password: string;
@@ -27,7 +27,23 @@ export const AuthContext = createContext({} as AuthContextData);
 export default function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>();
     const router = useRouter();
-    const isAuthenticated = !!user?.email;
+    const isAuthenticated = !!user;
+
+
+    useEffect(() => {
+        const {'nextauth.token': token } = parseCookies();
+        if (token) {
+            api.get('/me').then(response => {
+                const {email, permissions, roles} = response.data;
+
+
+                setUser({email, permissions, roles});
+            })
+        }
+
+    }, []);
+
+
 
     async function signIn({email, password}: SignInCredentials){
       try {
@@ -37,11 +53,24 @@ export default function AuthProvider({ children }: AuthProviderProps) {
               password
           });
           const {permissions, roles, token, refreshToken} = response.data;
+
+
+          setCookie(undefined, "nextauth.token", token, {
+              maxAge: 30 * 24 * 60 * 60, // 30 days
+                path: "/",
+          })
+          setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+            maxAge: 30 * 24 * 60 * 60, // 30 days
+            path: "/",
+        })
+
           setUser({
                 email,
                 permissions,
                 roles
           });
+
+          api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
           router.push("/dashboard");
       }
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,6 +3,9 @@ import { AuthContext } from "../context/AuthContext"
 
 export default function Dashboard() {
     const {user } = useContext(AuthContext)
+
+    
+
     return (
         <div>
             {user?.email}
